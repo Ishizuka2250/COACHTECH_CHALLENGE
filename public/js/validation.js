1f4e8f0812ac6094263373828c983f1c9fd29bdc
Ishizuka2250 <squid1028@gmail.com>
2021-10-09 23:58:35 +0900

    追加ファイル ・validation.js 変更ファイル ・contact.blade.php ・contact.css 実装 ・お問い合わせ画面のフロント側バリデーション

---
+++ public/js/validation.js
@@ -0,0 +1,60 @@
+function validation(targetID, inputValue, checkType, checkPattern = '', patternMessage = '') {
+  let targetErrorMessage = document.getElementsByClassName(targetID + '-error').item(0);
+  let errorMessage = '';
+
+  for (let type of checkType.split('|')) {
+    switch (type) {
+      case 'required':
+        if ((inputValue === '') && (errorMessage === '')) {
+          errorMessage = '必須入力です';
+        }else if((inputValue === '') && (errorMessage != '')) {
+          errorMessage += ' 必須入力です'
+        }break;
+
+      case 'pattern':
+        let reg = new RegExp(checkPattern)
+        if ((! reg.test(inputValue)) && (errorMessage === '')) {
+          errorMessage = patternMessage;
+        }else if((! reg.test(inputValue)) && (errorMessage != '')) {
+          errorMessage += ' ' + patternMessage;
+        }break;
+    }
+  }
+
+  if (errorMessage != '') {
+    targetErrorMessage.style.display = 'inline-block';
+    targetErrorMessage.innerHTML = errorMessage;
+    return false;
+  } else {
+    targetErrorMessage.style.display = 'none';
+    targetErrorMessage.innerHTML = '';
+    return true;
+  }
+}
+
+function blurInputCheck(event) {
+  let targetID = event.currentTarget.id;
+  let inputValue = event.currentTarget.value;
+  validation(targetID, inputValue, this.checkType, this.checkPattern, this.patternMessage);
+}
+
+function checkAndSubmit() {
+  let errorCount = 0;
+  if(! validation('firstname', document.getElementById('firstname').value, 'required')) errorCount++;
+  if(! validation('surname', document.getElementById('surname').value, 'required')) errorCount++;
+  if(! validation('email', document.getElementById('email').value, 'required|pattern', '^.*@.*\\..*', 'メールアドレスの形式で入力してください')) errorCount++;
+  if(! validation('postcode', document.getElementById('postcode').value, 'required|pattern', '[0-9]{3}-[0-9]{4}', '郵便番号の形式で入力してください')) errorCount++;
+  if(! validation('address', document.getElementById('address').value, 'required')) errorCount++;
+  if(! validation('opinion', document.getElementById('opinion').value, 'required')) errorCount++;
+  
+  if (errorCount === 0) document.contactform.submit();
+}
+
+document.getElementById('firstname').addEventListener('blur', {checkType: 'required', handleEvent: blurInputCheck});
+document.getElementById('surname').addEventListener('blur', {checkType: 'required', handleEvent: blurInputCheck});
+document.getElementById('email').addEventListener('blur', {checkType: 'required|pattern',checkPattern: '^.*@.*\\..*', patternMessage: 'メールアドレスの形式で入力してください', handleEvent: blurInputCheck});
+document.getElementById('postcode').addEventListener('blur', {checkType: 'required|pattern',checkPattern: '[0-9]{3}-[0-9]{4}', patternMessage: '郵便番号の形式で入力してください', handleEvent: blurInputCheck});
+document.getElementById('address').addEventListener('blur', {checkType: 'required', handleEvent: blurInputCheck});
+document.getElementById('opinion').addEventListener('blur', {checkType: 'required', handleEvent: blurInputCheck});
+
+
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,7 +11,7 @@
 @endsection
 
 @section('contents')
-  <form action="/check" method="POST">
+  <form action="/check" method="POST" name="contactform">
     @csrf
     <table>
       <tr>
@@ -19,13 +19,13 @@
         <td>
           <div class="name content-inner">
             <div class="input-name">
-              <input class="input-text" type="text" name="firstname" value="{{$firstname}}">
-              <span class="input-error-message"></span>
+              <input class="input-text" type="text" name="firstname" id="firstname" maxlength="100" value="{{$firstname}}">
+              <span class="firstname-error error-message" id="firstname-error"></span>
               <p class="example">例）山田</p>
             </div>
             <div class="input-name">
-              <input class="input-text" type="text" name="surname" value="{{$surname}}">
-              <span class="input-error-message"></span>
+              <input class="input-text" type="text" name="surname" id="surname" maxlength="100" value="{{$surname}}">
+              <span class="surname-error error-message" id="firstname-error"></span>
               <p class="example">例）太郎</p>
             </div>
           </div>
@@ -52,15 +52,14 @@
               <label class="input-radio-label" for="woman">女性</label>
             @endif
           </div>
-          <span class="input-error-message"></span>
         </td>
       </tr>
       <tr>
         <th><span class="required">メールアドレス</span></th>
         <td>
           <div class="content-inner">
-            <input class="input-text" type="email" name="email" id="" value="{{$email}}">
-            <span class="input-error-message"></span>
+            <input class="input-text" type="email" name="email" id="email" maxlength="100" value="{{$email}}">
+            <span class="email-error error-message"></span>
             <p class="example">例）test@example.com</p>
           </div>
         </td>
@@ -69,8 +68,8 @@
         <th><span class="required">郵便番号</span></th>
         <td>
           <div class="content-inner">
-            <input class="input-text" type="text" name="postcode" maxlength="8" id="" value="{{$postcode}}">
-            <span class="input-error-message"></span>
+            <input class="input-text" type="text" name="postcode" maxlength="8" id="postcode" value="{{$postcode}}">
+            <span class="postcode-error error-message"></span>
             <p class="example">例）123-4567</p>
           </div>
         </td>
@@ -79,8 +78,8 @@
         <th><span class="required">住所</span></th>
         <td>
           <div class="content-inner">
-            <input class="input-text" type="text" name="address" id="" value="{{$address}}">
-            <span class="input-error-message"></span>
+            <input class="input-text" type="text" name="address" id="address" maxlength="100" value="{{$address}}">
+            <span class="address-error error-message"></span>
             <p class="example">例）東京都渋谷区千駄ヶ谷1-2-3</p>
           </div>
         </td>
@@ -89,7 +88,7 @@
         <th><span>建物名</span></th>
         <td>
           <div class="content-inner">
-            <input class="input-text" type="text" name="building_name" id="" value="{{$building_name}}">
+            <input class="input-text" type="text" name="building_name" id="building_name" maxlength="100" value="{{$building_name}}">
             <p class="example">例）千駄ヶ谷マンション101</p>
           </div>
         </td>
@@ -98,12 +97,13 @@
         <th><span class="required">ご意見</span></th>
         <td>
           <div class="content-inner">
-            <textarea class="input-text feedback" name="opinion">{{$opinion}}</textarea>
-            <span class="input-error-message"></span>
+            <textarea class="input-text feedback" name="opinion" id="opinion" maxlength="120">{{$opinion}}</textarea>
+            <span class="opinion-error error-message"></span>
           </div>
         </td>
       </tr>
     </table>
-    <input class="button" type="submit" value="確認">
+    <script src="{{asset('js/validation.js')}}"></script>
+    <button type="button" class="button" onclick="checkAndSubmit()">送信</button>
   </form>
 @endsection
